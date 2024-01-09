cf245f8a83b63424b18eda1996269b0c8bbdd1c5
Yasuyuki ENDO <eyasuyuki@gmail.com>
2024-01-09 19:14:15 +0900

    add riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+      ProviderScope(
+          child: MyApp(),
+      ),
+  );
 }
 
 class MyApp extends StatelessWidget {
