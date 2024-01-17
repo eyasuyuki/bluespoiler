266e989227743477fa6f38020a2bdd141a13dd17
Yasuyuki ENDO <eyasuyuki@gmail.com>
2024-01-17 13:08:46 +0900

    add post button

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:bluespoiler/controller/spoiler_notifier.dart';
 import 'package:bluespoiler/page/spoiler_form.dart';
 import 'package:flutter/material.dart';
@@ -5,14 +7,15 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:image_picker_web/image_picker_web.dart';
 
 import 'data/data.dart';
 
 void main() {
   runApp(
-      ProviderScope(
-          child: MyApp(),
-      ),
+    ProviderScope(
+      child: MyApp(),
+    ),
   );
 }
 
@@ -28,7 +31,6 @@ class MyApp extends StatelessWidget {
 }
 
 class MyHomePage extends StatelessWidget {
-
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
@@ -39,28 +41,10 @@ class MyHomePage extends StatelessWidget {
         GlobalWidgetsLocalizations.delegate,
         GlobalCupertinoLocalizations.delegate
       ],
-      supportedLocales: const [
-        Locale('en', ''),
-        Locale('ja', '')
-      ],
-      title: "Bluespoiler",
+      supportedLocales: const [Locale('en', ''), Locale('ja', '')],
+      title: 'Bluespoiler',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
       home: _Page(title: 'Bluespoiler'),
@@ -72,6 +56,7 @@ class _Page extends HookWidget {
   _Page({required this.title});
 
   final String title;
+
   @override
   Widget build(BuildContext context) {
     // style
@@ -85,31 +70,37 @@ class _Page extends HookWidget {
     final inputController = useTextEditingController();
     inputController.addListener(() {
       spoiler.setInput(inputController.text);
-      filledText.value = spoiler.postText(
-          hiddenChar,
-          AppLocalizations.of(context)!.ellipsis);
+      filledText.value = spoiler.postText(hiddenChar, AppLocalizations.of(context)!.ellipsis);
     });
     // focus node
     final focusNode = useFocusNode();
+    // image bytes
+    final imageBytes = useState<Uint8List?>(null);
 
     void insertTextAtCursorPosition(String text) {
       final currentText = inputController.text;
       final cursorPosition = inputController.selection.baseOffset;
 
       // 現在のカーソル位置に文字を挿入
-      final newText = currentText.substring(0, cursorPosition) +
-          text +
-          currentText.substring(cursorPosition);
+      final newText = currentText.substring(0, cursorPosition) + text + currentText.substring(cursorPosition);
 
       // テキストを更新
       inputController.text = newText;
 
       // カーソル位置を更新
-      inputController.selection =
-          TextSelection.collapsed(offset: cursorPosition + text.length);
+      inputController.selection = TextSelection.collapsed(offset: cursorPosition + text.length);
 
       // ボタンが押された後にTextFieldにフォーカスをセット
       FocusScope.of(context).requestFocus(focusNode);
+    }
+
+    Future<void> pickImage() async {
+      try {
+        Uint8List? uint8list = await ImagePickerWeb.getImageAsBytes();
+        imageBytes.value = uint8list;
+      } catch (e) {
+        print(e);
+      }
     }
 
     return Scaffold(
@@ -120,9 +111,9 @@ class _Page extends HookWidget {
       body: Container(
         padding: EdgeInsets.all(5.0),
         child: Column(
-          //mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(AppLocalizations.of(context)!.description,
+          children: [
+            Text(
+              AppLocalizations.of(context)!.description,
               style: Theme.of(context).textTheme.headlineMedium,
             ),
             Text(AppLocalizations.of(context)!.help),
@@ -133,37 +124,53 @@ class _Page extends HookWidget {
               autofocus: true,
               controller: inputController,
               focusNode: focusNode,
-              decoration: InputDecoration(
-                counterText: '',
-                hintText: AppLocalizations.of(context)!.input_hint_text,
-                border: const OutlineInputBorder()
-              ),
+              decoration: InputDecoration(counterText: '', hintText: AppLocalizations.of(context)!.input_hint_text, border: const OutlineInputBorder()),
             ),
             Row(
               children: [
-                ElevatedButton(onPressed: () {
-                  insertTextAtCursorPosition('[');
-                },
-                    child: Text(AppLocalizations.of(context)!.open_bracket_input_text)
-                ),
-                ElevatedButton(onPressed: () {
-                  insertTextAtCursorPosition(']');
-                },
-                    child: Text(AppLocalizations.of(context)!.close_bracket_input_text)
-                )
+                ElevatedButton(
+                    onPressed: () {
+                      insertTextAtCursorPosition('[');
+                    },
+                    child: Text(AppLocalizations.of(context)!.open_bracket_input_text)),
+                ElevatedButton(
+                    onPressed: () {
+                      insertTextAtCursorPosition(']');
+                    },
+                    child: Text(AppLocalizations.of(context)!.close_bracket_input_text))
               ],
             ),
             Text(
               '${filledText.value}',
               style: Theme.of(context).textTheme.bodyMedium,
             ),
+            imageBytes.value != null
+                ? Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                    Image.memory(
+                      imageBytes.value!,
+                      width: 200,
+                      height: 200,
+                    ),
+                    CloseButton(onPressed: () {
+                      imageBytes.value = null;
+                    })
+                  ])
+                : ElevatedButton(
+                    onPressed: () async {
+                      await pickImage();
+                    },
+                    child: Text(AppLocalizations.of(context)!.image_button_text)),
+            Padding(padding: EdgeInsets.all(7.0)),
+            SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                onPressed: (inputController.text.isEmpty || imageBytes.value == null) ? null : () async {},
+                child: Text(AppLocalizations.of(context)!.post_button_text),
+              ),
+            )
           ],
         ),
-      ),// floatingActionButton: FloatingActionButton(
-      //   onPressed: _incrementCounter,
-      //   tooltip: 'Increment',
-      //   child: const Icon(Icons.add),
-      // ), // This trailing comma makes auto-formatting nicer for build methods.
+      ),
     );
   }
 }
